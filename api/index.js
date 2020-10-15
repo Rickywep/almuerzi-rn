3d091253524d21ba49382edb8ec0043f6c41aa48
Ricky Moreno <ricky.gnr.moreno@gmail.com>
2020-10-15 11:26:55 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const meals = require('./routes/meals')
+const orders = require('./routes/orders')
+const app = express()
+app.use(bodyParser.json())
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,  useUnifiedTopology: true })
+
+app.use('/api/meals', meals)
+app.use('/api/orders', orders)
+
+module.exports = app
